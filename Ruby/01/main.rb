d83256640f673d55d4ab303ba3aa20a8a39f586f
Nemestny <nemestny@gmail.com>
2018-07-01 12:39:15 +0500

    01 tets pry

--- Ruby/01/main.rb
+++ Ruby/01/main.rb
@@ -1,1 +1,20 @@
-puts 'Hello World!'
+require 'pry'
+
+class HelloWorld
+	def say
+		first = 5
+		second = 2
+		puts first + second
+		puts 'Hello World!'
+	end
+end
+
+class Runner
+	def start
+		hello = HelloWorld.new
+		hello.say		
+	end
+end
+
+run = Runner.new
+run.start
