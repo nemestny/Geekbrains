b0d4edbb3e84b1adca414e222b1ba28b4e471ba7
Aleksandr Noskov <nemestny@gmail.com>
2018-07-01 14:57:23 +0500

    01 Rubocop complete

--- Ruby/01/main.rb
+++ Ruby/01/main.rb
@@ -1,19 +1,19 @@
 require 'pry'
-
+# Helloworld
 class HelloWorld
-	def say
-		first = 5
-		second = 2
-		puts first + second
-		puts 'Hello World!'
-	end
+  def say
+    first = 5
+    second = 2
+    puts first + second
+    puts 'Hello World!'
+  end
 end
-
+# Runner
 class Runner
-	def start
-		hello = HelloWorld.new
-		hello.say		
-	end
+  def start
+    hello = HelloWorld.new
+    hello.say
+  end
 end
 
 run = Runner.new
