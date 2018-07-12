e85f5f872722e303b0dbd5002cefe9f18d78e447
Nemestny <nemestny@gmail.com>
2018-07-13 02:46:45 +0500

    04 hw 3

---
+++ Ruby/04_homework/task03.rb
@@ -0,0 +1,18 @@
+##
+# class of user
+class User
+  attr_accessor :name, :rank
+end
+
+user = User.new
+print 'Введите имя пользователя: '
+user.name = gets.strip
+
+until (1..5).cover?(rank ||= 0)
+  print 'Введите оценку пользователя (1-5): '
+  rank = gets.to_i
+end
+
+user.rank = rank
+
+p user
