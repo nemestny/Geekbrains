ee769d0990cb18e4224179469afab8a95c430c6c
Nemestny <nemestny@gmail.com>
2018-07-13 01:43:15 +0500

    04 walk (end)

--- Ruby/04/main.rb
+++ Ruby/04/main.rb
@@ -1,3 +1,14 @@
 require_relative 'tree'
 
-p TREE
+def walk(arr=[], &block)
+  arr.each do |x|
+    case x
+    when Hash
+      x.each { |_dir, files| walk(files, &block)}
+    when String
+      block.call x
+    end
+  end
+end
+
+walk(TREE) { |file| puts file}
