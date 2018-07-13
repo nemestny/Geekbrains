dbc7da329c541accff1b7f488f72a2358da55a0f
Aleksandr Noskov <nemestny@gmail.com>
2018-07-13 14:29:10 +0500

    ruby 04 hw 05 end

--- Ruby/04_homework/task05.rb
+++ Ruby/04_homework/task05.rb
@@ -1,11 +1,13 @@
+##
+# class of Array
 class Array
   def my_map
     out = []
     return unless block_given?
-    self.each { |e| out << yield(e) }
+    each { |e| out << yield(e) }
     out
   end
 end
 
-p [1,2,3,4].my_map { |i| i+1 }
-p %w[hello ruby world].my_map { |i| i.upcase }
+p [1, 2, 3, 4].my_map { |i| i + 1 }
+p %w[hello ruby world].my_map(&:upcase)
