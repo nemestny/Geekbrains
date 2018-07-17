7033b42937298a846d28807adb113945f234908a
Aleksandr Noskov <nemestny@gmail.com>
2018-07-17 21:05:07 +0500

    05 ruby homework task 05 what is work it?

--- Ruby/05_homework/task05.rb
+++ Ruby/05_homework/task05.rb
@@ -1,3 +1,15 @@
 class Factory
+  @@count = { bear: 0,
+              cube: 0,
+              ball: 0}
+  attr_reader :count
 
+  def create(type)
+    
+  end
+
+end
+
+class Toy
+  attr_accessor :type
 end
