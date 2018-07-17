b52cc18de7c5d610453767e8c319eb3884be3867
Aleksandr Noskov <nemestny@gmail.com>
2018-07-17 11:45:44 +0500

    05 ruby setters and getters

--- Ruby/05/main.rb
+++ Ruby/05/main.rb
@@ -1,23 +1,32 @@
 class Ticket
-  def initialize(price:, status:)
+  def initialize(date: '01.01.2018', price: 5.5)
     @price = price
-    @status = status
+    @date = date
+  end
+
+  # def set_price(price)
+  def price=(price)
+    @price = price
   end
 
   def price
     @price
   end
 
-  def status?
-    @status
+  def date=(date)
+    @date = date
   end
+
+  def date
+    @date
+  end
+
 end
 
-ticket = Ticket.new(price: 5.5, status: false)
+ticket1 = Ticket.new
+ticket2 = Ticket.new(price: 6.5, date: '02.03.1965')
 
-puts format('Цена билета %.2f',ticket.price)
-if ticket.status?
-  puts 'Билет продан'
-else
-  puts 'Билет доступен для покупки'
-end
+ticket1.price = 10.333
+
+puts "Цена билета ticket1: #{ticket1.price}, дата поездки #{ticket1.date}"
+puts "Цена билета ticket2: #{ticket2.price}, дата поездки #{ticket2.date}"
