211ee166603f610add8f1af41888a6b1f26dd0b8
Aleksandr Noskov <nemestny@gmail.com>
2018-07-17 19:50:02 +0500

    05 ruby homework task 04 middle 2

--- Ruby/05_homework/task04.rb
+++ Ruby/05_homework/task04.rb
@@ -4,26 +4,53 @@ class Settings
 
   # attr_accessor :block
   def initialize
-    @settings = {}
-    
+     @@settings = {}   
     def self.method_missing(name,*args)
-      puts name
-      p args
+      # puts name
+      # p args
+
       if name.to_s.include?('=')
-        @settings[name] = case args.first
+        key = name.to_s.delete('=').to_sym
+        @@settings[key] = case args.first
         when Array
           args[0][0]
         when String
           args[0]
         end
       end
+
     end
-    p @settings
+    p @@settings
     yield self if block_given?
-    p @settings
+    p @@settings
+
+    # create_methods @@settings
+    # p @@settings.class
+     class << self
+      @@settings.each do |method, name|
+        define_method method do
+          name
+        end
+      end
+    end
   end
+
+  # def create_methods settings
+  #   p settings
+  #   settings.each do |method, name|
+  #     define_method method do
+  #       name
+  #     end
+  #   end
+  # end
+
 end
 
 setting = Settings.new { |s| s.hello = 'world', s.bye = 'all', s.test = 'test'}
 
-# p setting.hello
+# p setting.methods
+
+p setting.hello
+p setting.bye
+p setting.test
+p setting.walk
