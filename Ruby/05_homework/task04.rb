6407292f61f04d91d7d1c96348ba05b0bbda1b89
Aleksandr Noskov <nemestny@gmail.com>
2018-07-17 20:16:07 +0500

    05 ruby homework task 04 end

--- Ruby/05_homework/task04.rb
+++ Ruby/05_homework/task04.rb
@@ -1,47 +1,40 @@
 ##
 # class of Settings
 class Settings
-
   def initialize
-     @@settings = {}   
-    def method_missing(name,*args)
+    @@settings = {}
+    def self.method_missing(name, *args)
       if name.to_s.include?('=')
         key = name.to_s.delete('=').to_sym
 
         @@settings[key] = case args.first
-        when Array
-          args[0][0]
-        when String
-          args[0]
-        end
-        
-      end
+                          when Array
+                            args[0][0]
+                          when String
+                            args[0]
+                          end 
+      end || super
     end
-    p @@settings
-    yield self if block_given?
-    p @@settings
 
-     class << self
-      @@settings.each do |method, name|
-        define_method method do
-          name
-        end
-      end
-    end
+    yield self if block_given?
+
+    class << self
+     @@settings.each do |method, name|
+       define_method method do
+         name
+       end
+     end
+   end
   end
 
   def method_missing(name)
-    puts 'sssssssssssssssss'
-  end
+    raise NameError, "Неверное имя параметра: #{name}"
+  end || super
 end
 
-setting = Settings.new { |s| s.hello = 'world', s.bye = 'all', s.test = 'test'}
+setting = Settings.new { |s| s.hello = 'world', s.bye = 'all', s.test = 'test' }
 
-# p setting.methods
-
-p setting.hello
-p setting.hello
 p setting.hello
 p setting.bye
-p setting.methods
+p setting.test
 p setting.walk
