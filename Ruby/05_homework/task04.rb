918c315813f645bbc419979094c8dbf005442fef
Aleksandr Noskov <nemestny@gmail.com>
2018-07-17 17:35:39 +0500

    05 ruby homework task 04 middle

--- Ruby/05_homework/task04.rb
+++ Ruby/05_homework/task04.rb
@@ -1,12 +1,29 @@
 ##
 # class of Settings
 class Settings
-  attr_accessor :block
-  def initialize (&block)
+
+  # attr_accessor :block
+  def initialize
+    @settings = {}
+    
+    def self.method_missing(name,*args)
+      puts name
+      p args
+      if name.to_s.include?('=')
+        @settings[name] = case args.first
+        when Array
+          args[0][0]
+        when String
+          args[0]
+        end
+      end
+    end
+    p @settings
     yield self if block_given?
+    p @settings
   end
 end
 
-setting = Settings.new { |s| s.hello = 'world'}
+setting = Settings.new { |s| s.hello = 'world', s.bye = 'all', s.test = 'test'}
 
-p setting.hello
+# p setting.hello
