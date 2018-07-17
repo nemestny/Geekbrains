983451cccbd767247da7b3f3fd01b74c49ae144f
Aleksandr Noskov <nemestny@gmail.com>
2018-07-17 09:49:09 +0500

    ruby 05 rainbow meta

--- Ruby/05/main.rb
+++ Ruby/05/main.rb
@@ -1,8 +1,21 @@
-class HelloWorld
-	define_method :cube do |my_arg|
-    my_arg ** 3
+class Rainbow
+  COLORS = {
+    red: 'красный',
+    orange: 'оранжевый',
+    yellow: 'желтый',
+    green: 'зеленый',
+    blue: 'голубой',
+    indigo: 'сининй',
+    violet: 'фиолетовый'
+  }
+
+  COLORS.each do |method, name|
+    define_method method do
+      name
+    end
   end
 end
 
-hello = HelloWorld.new
-puts hello.cube(2)
+r = Rainbow.new
+puts r.yellow
+puts r.red
