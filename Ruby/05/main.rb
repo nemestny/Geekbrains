bd2bdddf280a730dd3d755b3a07b2425b39c2aa9
Aleksandr Noskov <nemestny@gmail.com>
2018-07-17 12:39:37 +0500

    ruby 05 more namespaces part 03

--- Ruby/05/main.rb
+++ Ruby/05/main.rb
@@ -1,13 +1,12 @@
 class Worker
-  module Error
-    SALARY = 'Заработная плата должна быть больше нуля'
-    NAME = 'Введите корректное имя'
-  end
-
   def hello
     puts Error::SALARY
   end
+end
 
+class Worker::Error
+  SALARY = 'Заработная плата должна быть больше нуля'
+  NAME = 'Введите корректное имя'
 end
 
 puts Worker::Error::SALARY
