a02660c9d7f2a29e9c0b23fab95b90bdb15d6c7e
Aleksandr Noskov <nemestny@gmail.com>
2018-07-17 12:56:36 +0500

    05 ruby more singleton part 02

--- Ruby/05/main.rb
+++ Ruby/05/main.rb
@@ -1,19 +1,10 @@
 class HelloWorld
-  # def HelloWorld.say
-  def self.say
-    'hello'
-  end
+end
 
-  def instance_say
-    'instance hello'
+class << HelloWorld
+  def say
+    'hello'
   end
 end
 
-
-
 puts HelloWorld.say
-
-hello = HelloWorld.new
-puts hello.instance_say
-
-# puts HelloWorld.instance_say
