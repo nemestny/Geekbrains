f62435490bc66a7485657399f5e321ebf9605ae0
Aleksandr Noskov <nemestny@gmail.com>
2018-07-17 12:28:08 +0500

    05 ruby namespaces

--- Ruby/05/main.rb
+++ Ruby/05/main.rb
@@ -1,16 +1,16 @@
-class Ticket
-  attr_accessor :price, :date
+module Hello
+  class World
+    def say
+      puts 'Определяем метод say в первый раз'
+    end
+  end
 
-  def initialize(date: '01.01.2018', price: 5.5)
-    @price = price
-    @date = date
+  class World
+    def say
+      puts 'Определяем метод say во второй раз'
+    end
   end
 end
 
-ticket1 = Ticket.new
-ticket2 = Ticket.new(price: 6.5, date: '02.03.1965')
-
-ticket1.price = 10.333
-
-puts "Цена билета ticket1: #{ticket1.price}, дата поездки #{ticket1.date}"
-puts "Цена билета ticket2: #{ticket2.price}, дата поездки #{ticket2.date}"
+hello = Hello::World.new
+hello.say
