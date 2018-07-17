7705767dc5fac2bd3433ab5759712294f5dfd42c
Aleksandr Noskov <nemestny@gmail.com>
2018-07-17 20:34:30 +0500

    05 ruby homework task 04 final end

--- Ruby/05_homework/task04.rb
+++ Ruby/05_homework/task04.rb
@@ -3,33 +3,26 @@
 class Settings
   def initialize
     @@settings = {}
-    def self.method_missing(name, *args)
-      if name.to_s.include?('=')
-        key = name.to_s.delete('=').to_sym
 
-        @@settings[key] = case args.first
-                          when Array
-                            args[0][0]
-                          when String
-                            args[0]
-                          end 
-      end || super
+    def self.method_missing(name, *args)
+      key = name.to_s.delete('=').to_sym
+      @@settings[key] = args.flatten[0] || super
     end
 
     yield self if block_given?
 
     class << self
-     @@settings.each do |method, name|
-       define_method method do
-         name
-       end
-     end
-   end
-  end
+      @@settings.each do |method, name|
+        define_method method do
+          name
+        end
+      end
+    end
 
+  end
   def method_missing(name)
-    raise NameError, "Неверное имя параметра: #{name}"
-  end || super
+    raise NameError, "Неверное имя параметра: #{name}" || super
+  end
 end
 
 setting = Settings.new { |s| s.hello = 'world', s.bye = 'all', s.test = 'test' }
