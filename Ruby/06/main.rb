7dae5480634b71512b7588932bff83c49319f193
Aleksandr Noskov <nemestny@gmail.com>
2018-07-22 02:19:14 +0500

    06 ruby 03 comparable

--- Ruby/06/main.rb
+++ Ruby/06/main.rb
@@ -1,17 +1,31 @@
-module Hello
-  def say(name)
-    "Hello, #{name}!"
+class Ticket
+  include Comparable
+  attr_accessor :price, :date
+
+  def initialize(price:, date:)
+    @price = price
+    @date = date
+  end
+
+  def +(number)
+    @price += number
+    self
+  end
+
+  def <=>(ticket)
+    @price <=> ticket.price    
   end
 end
 
-class Greet
-  prepend Hello
-  def say(name)
-    "Greet: hi, #{name}!"
-  end
+ticket1 = Ticket.new(price: 5.5, date: '01.10.2018')
+ticket2 = Ticket.new(price: 5.5, date: '01.10.2018')
+
+ticket1 += 2
+
+if ticket1 > ticket2
+  puts 'Билет 1 дороже билета 2'
+elsif ticket1 < ticket2  
+  puts 'Билет 2 дороже билета 1'
+else
+  puts 'Билеты стоят одинаково'
 end
-
-greet = Greet.new
-puts greet.say('world')
-
-p Greet.ancestors
