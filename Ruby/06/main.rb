6cedb0381a77859f69e3dd398cc95ce00e720f8b
Aleksandr Noskov <nemestny@gmail.com>
2018-07-22 20:57:41 +0500

    06 ruby .send for calling private method

--- Ruby/06/main.rb
+++ Ruby/06/main.rb
@@ -1,13 +1,4 @@
-require 'singleton'
-
-class Factory
-  include Singleton
-end
-
-# factory = Factory.new
-# p factory
-
-factory1 = Factory.instance
-p factory1
-factory2 = Factory.instance
-p factory2
+HELLO = 1
+Object.send(:remove_const, :HELLO)
+HELLO = 2
+p HELLO
