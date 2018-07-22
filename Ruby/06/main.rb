ea66b4a2548ff87e1582c44a4f4e52706d2a4694
Aleksandr Noskov <nemestny@gmail.com>
2018-07-22 19:57:57 +0500

    06 ruby self 02

--- Ruby/06/main.rb
+++ Ruby/06/main.rb
@@ -1,15 +1,10 @@
 class Ticket
-  attr_accessor :price, :date
-
-  def initialize(price:, date:)
-    # @price = price
-    # @date = date
-    self.price = price
-    self.date = date
+  puts 'Класс Ticket'
+  puts self
+  module Summary
+    puts 'Модуль Summary'
+    puts self
   end
+  puts 'Вернемся в Ticket'
+  puts self
 end
-
-ticket = Ticket.new(price: 5.5, date: '01.10.2018')
-
-p ticket
-p Ticket.instance_methods(false)
