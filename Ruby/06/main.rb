14e4b4b758672752bab88787022bb6fe235486c4
Aleksandr Noskov <nemestny@gmail.com>
2018-07-22 12:48:55 +0500

    06 ruby include Enumerable (only each reqired)

--- Ruby/06/main.rb
+++ Ruby/06/main.rb
@@ -1,33 +1,16 @@
-module Duckable
-  def quack
-    'Кряк'
+class Rainbow
+  include Enumerable
+
+  def each
+    yield 'красный'
+    yield 'оранжевый'
+    yield 'желтый'
+    yield 'зеленый'
+    yield 'голубой'
+    yield 'синий'
+    yield 'фиолетовый'
   end
-
-  def swim
-    'Плавает как утка'
-  end
 end
 
-class Duck
-  include Duckable
-end
-
-class PseudoDuck
-  include Duckable
-end
-
-duck = Duck.new
-object = PseudoDuck.new
-ticket = Object.new
-
-def ticket.quack
-  'Кряк'
-end
-
-def ticket.swim
-  'Плавает как утка'
-end
-
-puts 'duck - это утка' if duck.respond_to?(:quack) && duck.respond_to?(:swim)
-puts 'object - это утка' if object.respond_to?(:quack) && object.respond_to?(:swim)
-puts 'ticket - это утка' if ticket.respond_to?(:quack) && ticket.respond_to?(:swim)
+r = Rainbow.new
+puts r.map(&:upcase)
