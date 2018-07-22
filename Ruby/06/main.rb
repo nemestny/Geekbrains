563e31f6255dd775a5bf2253eeb7edc5461e009b
Aleksandr Noskov <nemestny@gmail.com>
2018-07-22 20:31:14 +0500

    06 ruby private 02

--- Ruby/06/main.rb
+++ Ruby/06/main.rb
@@ -1,15 +1,21 @@
 class Ticket
   def price
-    5.5
+    price_format(5.5)
   end
-
-  private
 
   def date
     '01.10.2018'
   end
+
+  private
+
+  def price_format(price)
+    format('Цена билета %.2f',price)    
+  end
+
 end
 
 ticket = Ticket.new
 p ticket.price
 p ticket.date
+# p ticket.price_format(5.6)
