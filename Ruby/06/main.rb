40167efdc22d2bfb31f0519e7bdaf0adb854ed0c
Aleksandr Noskov <nemestny@gmail.com>
2018-07-22 20:50:57 +0500

    06 ruby private_class_method

--- Ruby/06/main.rb
+++ Ruby/06/main.rb
@@ -1,30 +1,5 @@
 class Person
-  attr_accessor :name
-
-  def initialize(name:, score:)
-   @name = name
-   @score = score
-  end
-
-  def compare(user)
-    if @score > user.score
-      'Выигрывает первый игрок'
-    elsif @score < user.score
-      'Выигрывает второй игрок'
-    else
-      'Ничья'
-    end
-  end
-
-  protected
-  # private
-
-  def score
-   @score
-  end
+  private_class_method :new
 end
 
-user1 = Person.new(name: 'Первый игрок', score: 12)
-user2 = Person.new(name: 'Второй игрок', score: 8)
-
-puts user1.compare(user2)
+user = Person.new
