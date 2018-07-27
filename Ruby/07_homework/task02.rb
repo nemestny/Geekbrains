3c564a5ce5a9bf0a1db955359ec1855f3d6e9d13
Aleksandr Noskov <nemestny@gmail.com>
2018-07-28 03:06:01 +0500

    07 homework task02..04 rubocop

--- Ruby/07_homework/task02.rb
+++ Ruby/07_homework/task02.rb
@@ -1,1 +1,1 @@
-p %w[first second third].map.with_index { |e,index| [e.to_sym,e[0].upcase+e[-2..-1]+" (#{index+1})"]}.to_h
+p %w[first second third].map.with_index { |e, index| [e.to_sym, e[0].upcase + e[-2..-1] + " (#{index + 1})"] }.to_h
