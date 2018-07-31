7cb286f23cc7220314a3262cf118fbaf341c43a9
Aleksandr Noskov <nemestny@gmail.com>
2018-07-31 16:03:01 +0500

    08 tests rspec stage 02

--- Ruby/08_tests/spec/example_spec.rb
+++ Ruby/08_tests/spec/example_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe 'Пример' do
-  it 'должен работать когда' do 
+  it 'должен работать в случае false' do 
     expect(false).to be_falsy
   end
-  it 'должен работать когда' do
+  it 'должен работать в случае true' do
     expect(true).to be_truthy
   end
 end
