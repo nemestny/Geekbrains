0d4eaf8029147fedd65fa92ad1f66808bfbbe75c
Aleksandr Noskov <nemestny@gmail.com>
2018-07-31 16:40:41 +0500

    08 tests rspec stage 05 callbacks

--- Ruby/08_tests/spec/callbacks_spec.rb
+++ Ruby/08_tests/spec/callbacks_spec.rb
@@ -1,1 +1,14 @@
-6:50
+require 'ostruct'
+
+RSpec.describe 'Объект' do
+  context 'ключ-значение' do
+    let(:object) do
+      OpenStruct.new(key: 'ключ', value: 'значение')
+    end
+
+    it 'должен содержать корректный ключ' do
+      expect(object.key).to eq('ключ')
+    end
+
+  end
+end
