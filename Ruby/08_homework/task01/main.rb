52337eeb27ef34fd612302d0b9a6d97b59dd8da2
Aleksandr Noskov <nemestny@gmail.com>
2018-08-01 15:11:37 +0500

    08 ruby homework prepare app for file scan

--- Ruby/08_homework/task01/main.rb
+++ Ruby/08_homework/task01/main.rb
@@ -2,14 +2,16 @@ require 'pathname'
 
 def scan(path,counter = 0)
   entries = Dir.new(path).reject { |x| %w[. ..].include? x }.map { |x| path.join x  }
-  p entries
-  entries.each do |item|
-    counter = scan(item,counter) if File.directory? item
-    counter +=1 if item.extname == '.rb'
+  entries.reduce(counter) do |counter, item|
+    if File.directory? item
+      scan(item, counter)
+    elsif item.extname == '.rb'
+      counter + 1
+    else
+      counter
+    end
   end
-  counter
 end
 
 path = Pathname.new('c:\Projects\Geekbrains\Ruby')
 p scan(path)
-# p counter
