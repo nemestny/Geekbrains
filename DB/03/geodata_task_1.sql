1354c0936cf0105b025aa1109613e4b79e5eb7b5
Aleksandr Noskov <nemestny@gmail.com>
2018-08-23 10:51:18 +0500

    03 DB HW init task 1.1 end

--- DB/03/geodata_task_1.sql
+++ DB/03/geodata_task_1.sql
@@ -2,8 +2,8 @@ use `geodata`;
 
 select co.title as Country, re.title as Region, ci.title as City
 from _countries co
-	left join _regions re
-		on co.id = re.country_id
 	left join _cities ci
+		on co.id = ci.country_id
+	left join _regions re
 		on re.id = ci.region_id
-	where ci.title = 'Москва';
+where ci.title = 'Москва';
