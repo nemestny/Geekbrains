f1be91f93d3b44052d0bd52016c73a45f132919b
Aleksandr Noskov <nemestny@gmail.com>
2018-08-23 11:28:30 +0500

    03 DB HW init task 2.1 add sort

--- DB/03/employees_task_1.sql
+++ DB/03/employees_task_1.sql
@@ -1,9 +1,10 @@
 use `employees`;
 
-select d.dept_name, avg(s.salary)
-	from departments d
-		left join dept_emp de
-			on d.dept_no = de.dept_no
-		left join salaries s
-			on de.emp_no = s.emp_no
-		group by de.dept_no;
+select d.dept_name, avg(s.salary) avg_salary
+from departments d
+	left join dept_emp de
+		on d.dept_no = de.dept_no
+	left join salaries s
+		on de.emp_no = s.emp_no
+group by de.dept_no
+order by avg_salary desc;
