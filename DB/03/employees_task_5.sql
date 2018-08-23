2572e048008ddf6b28737c19d31d51c8f2caf318
Aleksandr Noskov <nemestny@gmail.com>
2018-08-23 15:12:37 +0500

    03 DB HW init task 2.5 add sum salary an order

--- DB/03/employees_task_5.sql
+++ DB/03/employees_task_5.sql
@@ -1,8 +1,11 @@
 use `employees`;
 
-select de.dept_no, d.dept_name, count(de.emp_no)
+select de.dept_no, d.dept_name, count(de.emp_no) sum_employees, sum(s.salary) sum_salary
 from dept_emp de
 left join departments d
 	on d.dept_no = de.dept_no
-where de.to_date = '9999-01-01'
-group by dept_no;
+left join salaries s
+	on s.emp_no = de.emp_no
+where s.to_date = '9999-01-01' and de.to_date = '9999-01-01'
+group by de.dept_no
+order by sum_salary desc;
